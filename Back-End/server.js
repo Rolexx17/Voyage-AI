41787672e6fd089d77440e44ac09fd71e1c734df
Cherish Evangeline <cherish.evangeline@students.mikroskil.ac.id>
2026-05-19 20:28:38 +0700

    Update server.js

--- Back-End/server.js
+++ Back-End/server.js
@@ -21,6 +21,8 @@ app.use(cors());
 app.use(express.json());
 
 // Logger sederhana untuk memantau request
+//vjfnvbjb
+//mvfkmvb
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
